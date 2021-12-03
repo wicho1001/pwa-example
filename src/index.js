90374bd72d00c0b06377c9c7dc578cd499ddb426
wicho1001 <wicho@DESKTOP-TNAVEJP.localdomain>
2021-12-03 13:17:54 -0600

    feat: pwa ignition :rocket:

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { register } from './serviceWorker';
+
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,6 +12,8 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
